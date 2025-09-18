2203bcc1bcc058356c328da69f7e8b40f9fb6f1c
Ronan Mezerette <e4997u@etu.univ-lorraine.fr>
2025-09-18 11:21:19 +0200

    added project version 1.0 and variables in HelloProjConfig

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <HelloProjconfig.h>
+
 void hello_world() {
 	std::cout << "Hello World!\n";
+}
+
+void print_version(){
+	std::cout << "project version is " << HelloProj_VERSION_MAJOR << "." << HelloProj_VERSION_MINOR << std::endl;
 }
 
 int main()
 {
 	hello_world();
+	print_version();
 }
